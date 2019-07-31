6a4c5c279c64f8c9abd9f2b2f8c5e463132bd48b
Stafford Williams <stafford.williams@gmail.com>
2019-07-31 21:34:17 +1000

    test

--- src/UnitTest1.cs
+++ src/UnitTest1.cs
@@ -1,7 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.IO;
 using System.Reflection;
@@ -17,12 +18,32 @@ namespace AzureWebMonitor.Test
             using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), new ChromeOptions { }))
             {
                 driver.Navigate().GoToUrl(@"http://azure.com");
-                // var link = driver.FindElement(By.PartialLinkText("TFS Test API"));
+                //var link = driver.FindElement(By.PartialLinkText("TFS Test API"));
                 // var jsToBeExecuted = $"window.scroll(0, {link.Location.Y});";
                 // ((IJavaScriptExecutor)driver).ExecuteScript(jsToBeExecuted);
-                // var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
-                // var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("TFS Test API")));
-                // clickableElement.Click();
+                var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Pricing")));
+                clickableElement.Click();
+
+                clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Pricing by product")));
+                clickableElement.Click();
+
+                var searchBox = driver.FindElement(By.Id("searchPicker-input"));
+                Actions actions = new Actions(driver);
+                actions.MoveToElement(searchBox);
+                actions.Perform();
+
+                searchBox.SendKeys("signalr");
+
+                clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("Azure SignalR Service")));
+                clickableElement.Click();
+
+
+
+
+
+
+
             }
         }
     }
