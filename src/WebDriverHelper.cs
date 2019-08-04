5d3277549dc1bd1f162d0f924b56a6c5b9c7359b
Stafford Williams <stafford.williams@gmail.com>
2019-08-04 22:24:08 +1000

    block urls

--- src/WebDriverHelper.cs
+++ src/WebDriverHelper.cs
@@ -2,15 +2,21 @@ using System.IO;
 using System.Reflection;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Linq;
 
 namespace AzureWebMonitor.Test
 {
     public static class WebDriverHelper 
     {
-        public static IWebDriver GetDriver() 
+        static readonly string[] _blockUrls = new [] {
+            "www.google-analytics.com"
+        };
+
+        public static ChromeDriver GetDriver() 
         {
             var options = new ChromeOptions();
-            options.AddArgument("--host-resolver-rules=MAP www.google-analytics.com 127.0.0.1");
+            var rules = string.Join(',', _blockUrls.Select(r => $"MAP {r} 127.0.0.1"));
+            options.AddArgument($"--host-resolver-rules={rules}");
             options.AddArgument("--headless");
             return new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
         }
